4096a774a2db2f329954424cfd81b7671a00a905
nomisrmugisa <nomisrmugisa@gmail.com>
2023-11-15 13:28:26 +0300

    clean up

--- fields_updater.sh
+++ fields_updater.sh
@@ -4,7 +4,8 @@ file_path=$1 # Use a relative path if the file is in the same directory
 
 new_content=$(echo -e "$2")
 result=$(echo -e "$new_content")
-#result=$(echo "$result" | sed 's/""/"/g')
+result=$(echo -e "$result" | sed 's/""/"/g')
+result=$(echo -e "$new_content" | sed 's/^"\(.*\)"$/\1/g; s/\\//g')
 
 if ! grep -q "// Nira generated fields //" "$file_path"; then
     # Add the new content to the top of the file and exit
